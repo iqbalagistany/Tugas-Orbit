c02d528757ce03549c3700ca0de6278c5895d806
iqbalagistany <iqbal.agistany@gmail.com>
2022-03-05 22:27:25 +0800

    upload tugas

---
+++ menu.py
@@ -0,0 +1,22 @@
+menu = open('menu.txt','w')
+
+menu.write('''====SELAMAT DATANG DI WARKOP AGISTA=====
+
+==Berikut menu yang tersedia==
+
+===== MAKANAN =====
+- Pisang Goreng = \tRp 2000
+- Tempe Mendoan = \tRp 1000
+- Indomie Goreng = \tRp 5000
+- Indomie Rebus = \tRp 6000
+- Nasi Kucing = \tRp 3000
+
+===== MINUMAN =====
+- Kopi Tubruk = \tRp 5000
+- Wedang Ronde = \tRp 8000
+- Susu Jahe = \tRp 6000
+- Susu Hangat = \tRp 4000
+- Teh Tawar = \tRp 3000
+ ''')
+
+menu.close()
